82779c1ed7d1f76f2394db38277fe33bf84b9799
kostoo <nesterow14@yandex.ru>
2022-03-01 14:26:11 +0300

    Initial commit

---
+++ src/main/java/com/managerPass/ManagerPassApplication.java
@@ -0,0 +1,13 @@
+package com.managerPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managerPass/ManagerPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
